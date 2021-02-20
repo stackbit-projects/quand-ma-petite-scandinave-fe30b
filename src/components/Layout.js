89748c3e40e0b882c882cd50f0ffce5700bd4be8
Stackbit Code Editor <support@stackbit.com>
2021-02-20 18:44:27 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,9 +15,6 @@ export default class Body extends React.Component {
                     <title>
                         {_.get(this.props, 'pageContext.frontmatter.title', null)}
                     </title>
-                    htmlAttributes={{
-                        lang: 'en',
-                    }}
                     <meta charSet="UTF-8"/>
                     <meta name="viewport" content="width=device-width, initialScale=1.0"/>
                     <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
